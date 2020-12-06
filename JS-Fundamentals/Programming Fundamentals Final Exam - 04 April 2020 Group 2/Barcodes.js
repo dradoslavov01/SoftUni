43281c95467eb624b56d373d1e44dce42d6afae6
dradoslavov01 <daniradoslavov01@abv.bg>
2020-12-06 20:07:43 +0200

    Added exercise

---
+++ JS-Fundamentals/Programming Fundamentals Final Exam - 04 April 2020 Group 2/Barcodes.js
@@ -0,0 +1,23 @@
+function solve(input) {
+  let n = input.shift();
+
+  while (n != 0) {
+    let line = input.shift();
+    let regex = /(@#+)(?<product>[A-Z][A-Za-z0-9]{4,}[A-Z])\1/g;
+    let match = regex.exec(line);
+    if (match) {
+      let product = match.groups.product;
+      let matches = product.match(/\d+/g);
+      if (matches != null) {
+        console.log(`Product group: ${matches.join("")}`);
+      } else {
+        console.log("Product group: 00");
+      }
+    } else {
+      console.log("Invalid barcode");
+    }
+    n--;
+  }
+}
+
+solve(["3", "@#FreshFisH@#", "@###Brea0D@###", "@##Che46sE@##"]);
