d81f5428a9a723c7513ae2cec46236ec38a9f03d
dradoslavov01 <daniradoslavov01@abv.bg>
2021-01-12 21:57:57 +0200

    Unfinished task

---
+++ JS-Advanced/Exercise Syntax Functions and Statements/Time.js
@@ -0,0 +1,9 @@
+function solve(steps, meters, speed) {
+    let seconds = 0;
+    let minutes = 0;
+    let hours = 0;
+    let metersPerSecond = (speed * 1000) / 60 / 60;
+ 
+    
+}
+solve(4000, 0.60, 5)
