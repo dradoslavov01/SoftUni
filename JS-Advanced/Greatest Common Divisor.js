f56d71cf6098a93f87450ed51448b681d96557b6
dradoslavov01 <daniradoslavov01@abv.bg>
2021-01-12 12:42:12 +0200

    Edited exercise

---
+++ JS-Advanced/Greatest Common Divisor.js
@@ -0,0 +1,13 @@
+function solve(num1, num2) {
+
+    num1 = Math.abs(num1);
+    num2 = Math.abs(num2);
+    if (num2 > num1) {var temp = num1; num1 = num2; num2 = temp;}
+    while (true) {
+        if (num2 == 0) return num1;
+        num1 %= num2;
+        if (num1 == 0) return num2;
+        num2 %= num1;
+    }
+}
+console.log(solve(2154, 458)); 
