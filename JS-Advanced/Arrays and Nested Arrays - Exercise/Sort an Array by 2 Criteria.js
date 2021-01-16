b3270b0a3a3d3849b048c6f8b4321b74b0a81731
dradoslavov01 <daniradoslavov01@abv.bg>
2021-01-16 15:37:19 +0200

    Added exercise

---
+++ JS-Advanced/Arrays and Nested Arrays - Exercise/Sort an Array by 2 Criteria.js
@@ -0,0 +1,10 @@
+function solve(input) {
+  let result = input.sort((a, b) => a.length - b.length || a.localeCompare(b));
+  return result.join('\n');
+}
+console.log(solve(['Isacc', 
+'Theodor', 
+'Jack', 
+'Harrison', 
+'George']
+));
