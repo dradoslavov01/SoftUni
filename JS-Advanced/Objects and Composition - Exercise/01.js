d21d91d46bbdc93544c3b84aab555d68550e02de
dradoslavov01 <daniradoslavov01@abv.bg>
2021-01-19 18:42:34 +0200

    Added exercise

---
+++ JS-Advanced/Objects and Composition - Exercise/01.js
@@ -0,0 +1,10 @@
+function solve(input) {
+    const obj = {};
+    for (let i = 0; i < input.length; i++) {
+        if (i % 2 == 0) {
+            obj[input[i]] = +input[i + 1];
+        }
+    }
+    return obj
+}
+console.log(solve(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']));
