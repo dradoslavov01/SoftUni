38f64116cacf71118e0ce54c2dd2a6d333f07fc3
dradoslavov01 <daniradoslavov01@abv.bg>
2021-01-22 20:38:39 +0200

    Added exercise

--- JS-Advanced/DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ JS-Advanced/DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,18 @@
 function search() {
-   // TODO
+  const input = document.querySelector("#searchText").value;
+
+  const towns = document.querySelector("#towns");
+
+  let matches = 0;
+
+  for (const town of Array.from(towns.children)) {
+    let townName = town.textContent;
+
+    if (townName.includes(input)) {
+      matches++;
+      town.style.fontWeight = "bold";
+      town.style.textDecoration = "underline";
+    }
+  }
+  document.querySelector("#result").textContent = `${matches} matches found`;
 }
