dbe9deea9254315d9f88b7def26ec6d09dc3fb1b
dradoslavov01 <daniradoslavov01@abv.bg>
2021-01-22 20:13:03 +0200

    Added exercise

--- JS-Advanced/DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
+++ JS-Advanced/DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
@@ -1,3 +1,10 @@
 function toggle() {
-    console.log('TODO:...');
+  let display = document.getElementsByClassName("button")[0].textContent;
+  if (display === "More") {
+    document.getElementById("extra").style.display = "block";
+    document.getElementsByClassName("button")[0].textContent = "Less";
+  } else {
+    document.getElementById("extra").style.display = "none";
+    document.getElementsByClassName("button")[0].textContent = "More";
+  }
 }
