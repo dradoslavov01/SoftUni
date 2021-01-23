639502af2f8ed1a4beb921038270671674f8d65c
dradoslavov01 <daniradoslavov01@abv.bg>
2021-01-23 18:41:21 +0200

    Added exercise

--- JS-Advanced/DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Serach Engine/solution.js
+++ JS-Advanced/DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Serach Engine/solution.js
@@ -1,8 +1,16 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
+   const rows = document.querySelectorAll('tbody tr');
 
    function onClick() {
-      //   TODO:
-
+      const input = document.querySelector('#searchField').value.toLowerCase();
+      
+      for (const row of rows) {
+         if (row.textContent.toLowerCase().includes(input)) {
+            row.setAttribute('class', 'select');
+         } else {
+            row.removeAttribute('class');
+         }
+      }
    }
 }
