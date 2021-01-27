3257c182a90b3b7d819357bbcbeb7ff8d3825128
dradoslavov01 <daniradoslavov01@abv.bg>
2021-01-27 21:41:36 +0200

    Added exercise

--- JS-Advanced/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
+++ JS-Advanced/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -1,17 +1,16 @@
 function lockedProfile() {
-    document.getElementById('main').addEventListener('click', (e) => {
-        if (e.target.tagName === 'BUTTON') {
-            const profile = e.target.parentNode;
-            const isLokced = profile.querySelector('input[type=radio]:checked').value === 'lock';
-            if (isLokced) {
-                return;
-            } 
-            let div = profile.querySelector('div');
-            let isVisible = div.style.display === 'block';
-            div.style.display = isVisible ? 'none' : 'block';
-            e.target.textContent = !isVisible ? 'Hite it' : 'Show more';
-        }
-        
-    })
-    
+  document.getElementById("main").addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+      const profile = e.target.parentNode;
+      const isLokced =
+        profile.querySelector("input[type=radio]:checked").value === "lock";
+      if (isLokced) {
+        return;
+      }
+      let div = profile.querySelector("div");
+      let isVisible = div.style.display === "block";
+      div.style.display = isVisible ? "none" : "block";
+      e.target.textContent = !isVisible ? "Hide it" : "Show more";
+    }
+  });
 }
--- JS-Advanced/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ JS-Advanced/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+    document.getElementById('newItemText').value;
+    document.getElementById('newItemValue').value;
+    const select = document.getElementById('menu');
+    const option = document.createElement('option');
+    option.textContent = document.getElementById('newItemText').value;
+    option.value = document.getElementById('newItemValue').value;
+    select.appendChild(option);
+    document.getElementById('newItemText').value = '';
+    document.getElementById('newItemValue').value = '';
 }
