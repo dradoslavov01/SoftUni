57821f222f19691fd41a475a6ed29cbc9b28b275
dradoslavov01 <daniradoslavov01@abv.bg>
2021-01-29 19:11:43 +0200

    Added exercise

--- JS-Advanced/Advanced Functions - Exercise/02.js
+++ JS-Advanced/Advanced Functions - Exercise/02.js
@@ -1,24 +1,26 @@
 function solve(...args) {
-  /* let output = ''; */
-  args.forEach((ele) => {
-    console.log(`${typeof ele}: ${ele}`);
-  });
-  let types = {};
-  args.forEach((ele) => {
-    if (!types.hasOwnProperty(typeof ele)) {
-      types[typeof ele] = 1;
-    } else {
-      types[typeof ele] += 1;
-    }
-  });
-  let keys = Object.keys(types);
+   /* let output = ''; */
+   args.forEach(ele => {
+      console.log(`${typeof ele}: ${ele}`);
+   })  
+   let types = {};
+   args.forEach(ele =>{
+      if(!types.hasOwnProperty(typeof ele)){
+         types[typeof ele] = 1;
+      }else{
+         types[typeof ele] += 1;
+      }
+   })
+   let keys = Object.keys(types);
 
-  keys.sort((a, b) => types[b] - types[a]);
+   keys.sort((a,b) => 
+      types[b] - types[a]
+   );
 
-  for (let set of keys) {
-    console.log(`${set} = ${types[set]}`);
-  }
+   for(let set of keys){
+      console.log(`${set} = ${types[set]}`);
+   }
+
+   
 }
-solve("cat", 42, 32, function () {
-  console.log("Hello world!");
-});
+solve('cat', 42, 32, function () { console.log('Hello world!'); });
--- JS-Advanced/Advanced Functions - Exercise/03.js
+++ JS-Advanced/Advanced Functions - Exercise/03.js
@@ -1,11 +1,26 @@
 function solve() {
     let n1 = 0;
     let n2 = 1;
-
-     b 
-
-    
+    let nextN;
+    function nextNum() {
+        nextN = n1 + n2;
+        n1 = n2;
+        n2 = nextN;
+        return n1;
+    }
+    return nextNum;
 }
+let func = solve();
+console.log(func());
+console.log(func());
+console.log(func());
+console.log(func());
+console.log(func());
 
 
-// 0 1 2 3 5 8 13
+
+
+
+
+
+
