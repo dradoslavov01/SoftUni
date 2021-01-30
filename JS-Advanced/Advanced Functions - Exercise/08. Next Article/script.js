ba8eddc4d02e3ffdbc04677c953ed7720ba4b400
dradoslavov01 <daniradoslavov01@abv.bg>
2021-01-30 18:36:52 +0200

    Problem solving

--- JS-Advanced/Advanced Functions - Exercise/08. Next Article/script.js
+++ JS-Advanced/Advanced Functions - Exercise/08. Next Article/script.js
@@ -1,3 +1,12 @@
 function getArticleGenerator(articles) {
-    // TODO
+    const div = document.getElementById('content');
+    function showNext() {
+        if(articles.length > 0) {
+           const text = articles.shift(); 
+           const content = document.createElement('article'); 
+           content.textContent = text;
+           div.appendChild(content);
+        }          
+    }
+    return showNext;
 }
