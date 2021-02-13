b692bdc6ad305dad7a7bb4ccbfb3c1a75a746da2
dradoslavov01 <daniradoslavov01@abv.bg>
2021-02-13 20:27:44 +0200

    Added exercise

---
+++ JS-Advanced/JS Advanced - Exam Prep 1 - 12.02.2021/03/03.test.js
@@ -0,0 +1,21 @@
+const pizzUni = require('./03');
+const { assert } = require("chai");
+
+describe("pizzUni", function() {
+
+    it('Make an order', () => {
+        let pizza = { orderedPizza: 'pizza', orderedDrink: 'drink' };
+        let pizza2 = { orderedPizza: 'pizza' };
+
+        let pizza1 = {orderedDrink: 'drink'};
+        let pizza3 = {};
+
+        assert.throw(() => pizzUni.makeAnOrder(pizza1), 'You must order at least 1 Pizza to finish the order.');
+        assert.throw(() => pizzUni.makeAnOrder(pizza3), 'You must order at least 1 Pizza to finish the order.');
+
+        assert.equal(pizzUni.makeAnOrder(pizza2), `You just ordered ${pizza2.orderedPizza}`);
+        assert.equal(pizzUni.makeAnOrder(pizza), `You just ordered ${pizza.orderedPizza} and ${pizza.orderedDrink}.`);
+
+
+    })
+});
