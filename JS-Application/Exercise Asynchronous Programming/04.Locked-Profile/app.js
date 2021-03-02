571c9277375c51f83801376dcc3a28f35d0c36f8
dradoslavov01 <daniradoslavov01@abv.bg>
2021-03-02 12:58:21 +0200

    Added exercise

--- JS-Application/Exercise Asynchronous Programming/04.Locked-Profile/app.js
+++ JS-Application/Exercise Asynchronous Programming/04.Locked-Profile/app.js
@@ -1,3 +1,35 @@
 function lockedProfile() {
-    console.log('TODO...')
+  const [lock, unlock, userName, email, age] = document.querySelectorAll(
+    "input"
+  );
+  const info = document.getElementById("user1HiddenFields");
+  userName.value = "Mary";
+
+  const button = document.querySelector("button");
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const isLokced =
+      document.querySelector("input[type=radio]:checked").value === "lock";
+    if (isLokced) {
+      return;
+    }
+
+    const info = document.getElementById("user1HiddenFields");
+    info.style.display = "block";
+    console.log(email);
+    getData();
+  });
+
+  async function getData() {
+    const url = "http://localhost:3030/jsonstore/advanced/profiles";
+    const response = await fetch(url);
+    const data = await response.json();
+    Object.values(data).forEach((e) => {
+      if (userName.value == e.username) {
+        email.value = e.email;
+        age.value = e.age;
+      }
+    });
+  }
 }
